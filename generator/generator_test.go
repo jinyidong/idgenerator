86141550e4bfaf6edd6717cc43c16fed4b82c1ac
jinyidong <jinyidong@outlook.com>
2019-03-15 10:01:13 +0800

    update

--- generator/generator_test.go
+++ generator/generator_test.go
@@ -3,11 +3,9 @@ package generator
 import (
 	"fmt"
 	"testing"
+	"time"
 )
 
-func TestGenerate(t *testing.T) {
-	GetId("test")
-}
 
 func TestGetId(t *testing.T) {
 	id,err:=GetId("test")
@@ -15,4 +13,41 @@ func TestGetId(t *testing.T) {
 		t.Error(err)
 	}
 	fmt.Println(id)
+}
+
+//generate with unconfig tag
+func TestGetId3(t *testing.T) {
+	ch:=make(chan int64,100)
+	generate("unconfig",ch,time.Now().String())
+	tt := time.NewTicker(time.Second)
+Loop:
+	for{
+		select {
+		case id:=<-ch:
+			fmt.Println(id)
+		case <-tt.C:
+			fmt.Println("timeout...break loop")
+			break Loop
+		}
+	}
+	fmt.Println("end")
+}
+
+
+//generate with test tag
+func TestGetId2(t *testing.T) {
+	ch:=make(chan int64,100)
+	generate("test",ch,time.Now().String())
+	tt := time.NewTicker(time.Second)
+	Loop:
+	for{
+		select {
+		case id:=<-ch:
+			fmt.Println(id)
+		case <-tt.C:
+			fmt.Println("timeout...break loop")
+			break Loop
+		}
+	}
+	fmt.Println("end")
 }
